328cba0902efac6686c586efafb6b45ae2be7e50
derecost <denise.reis@gmail.com>
2014-08-23 09:45:10 -0300

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,12 +1,13 @@
 #################################################################
-Course Project - Getting and Cleaning data
+#######################Course Project############################
 #################################################################
+#Change the working directory
+setwd("C:\\Users\\Denise\\Documents\\Arquivos_Denise\\Cursos\\Curso R\\R - Coursera\\Getting and cleaning data\\course project")
 
-setwd("C:\\Users\\Denise\\Documents\\Arquivos_Denise\\Cursos\\R - Coursera\\Getting and cleaning data\\course project")
 #################################################################
-#1)Merges the training and the test sets to create one data set.#
+####################Task 1#######################################
 #################################################################
-#reading the files
+#Read the files
 subj.train<-read.table("./UCI HAR Dataset/subject_train.txt")
 x.train<-read.table("./UCI HAR Dataset/X_train.txt")
 y.train<-read.table("./UCI HAR Dataset/y_train.txt")
@@ -16,37 +17,51 @@ y.test<-read.table("./UCI HAR Dataset/y_test.txt")
 act.labels<-read.table("./UCI HAR Dataset/activity_labels.txt")
 features<-read.table("./UCI HAR Dataset/features.txt")
 
+#Merge the training and the test sets 
 train.data<-cbind(subj.train,y.train,x.train)
 test.data<-cbind(subj.test,y.test,x.test)
-data<-rbind(train.data,test.data) #data set created with the training and the test information
-  
+
+#Create one data set
+data<-rbind(train.data,test.data) 
+
 #################################################################
-#2)Extracts only the measurements on the mean and standard deviation for each measurement. 
+####################Task 2#######################################
 #################################################################
+#Extract only the measurements on the mean and standard deviation for each measurement 
 extract.cols<-c(grep("mean()",features[,2]),grep("std()",features[,2]))
+
+#Create a new data set with these columns
 data2<-data[,c(1,2,extract.cols+2)]
 
 #################################################################
-#3)Uses descriptive activity names to name the activities in the data set
+####################Task 3#######################################
 #################################################################
+#Use descriptive activity names to name the activities in the data set
 data2$act.description<-as.factor(data2[,2])
 levels(data2$act.description)<-as.character(relevel(act.labels[,2],ref=1))
-table(data2$act.description,data2[,2]) #check if the order is ok
+
+#Check if the order is ok (optional)
+table(data2$act.description,data2[,2]) 
 
 #################################################################
-#4)Appropriately labels the data set with descriptive variable names. 
+####################Task 4#######################################
 #################################################################
+#Label the data set with descriptive variable names. 
 names(data2)<-c("Subject","ActivityCode",c(as.character(relevel(features[c(extract.cols),2],ref=1))),"Activity")
 
 #################################################################
-#5)Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
+####################Task 5#######################################
 #################################################################
 library(data.table)
 
+#Take the average of each variable for each activity and each subject. 
 dt<-as.data.table(data2)
 dt2 <- dt[, lapply(.SD,mean), by=list(data2$Subject,data2$Activity)]
 tiny.data<-as.data.frame(dt2)
-tiny.data<-tiny.data[,c(1,2,4:82)]
-names(tiny.data)<-c("Subject","Activity",c(as.character(relevel(features[c(extract.cols),2],ref=1))))
 
+#Create an independent tidy data set
+tiny.data<-tiny.data[,c(1,2,4:82)]
+names(tiny.data)<-c("Subject","Activity",paste("Average",c(as.character(relevel(features[c(extract.cols),2],ref=1))),sep="-"))
+
+#Create a txt file
 write.table(tiny.data,file="tiny.data.txt",row.names=F)
